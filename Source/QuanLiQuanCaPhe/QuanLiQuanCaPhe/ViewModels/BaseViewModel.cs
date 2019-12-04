8cdc46ab1ad835768270264332a4b7a9ffea452d
Doan Phuc <mrjared16>
2019-12-04 23:26:40 +0700

    add new OnPropertyChanged function for assign value

--- Source/QuanLiQuanCaPhe/QuanLiQuanCaPhe/ViewModels/BaseViewModel.cs
+++ Source/QuanLiQuanCaPhe/QuanLiQuanCaPhe/ViewModels/BaseViewModel.cs
@@ -7,16 +7,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace QuanLiQuanCaPhe.ViewModel
+namespace QuanLiQuanCaPhe.ViewModels
 {
 	public class BaseViewModel : INotifyPropertyChanged
 	{
 		public event PropertyChangedEventHandler PropertyChanged;
-
 		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
 		{
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
 		}
+        protected virtual bool OnPropertyChanged<T>(ref T backingField, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(backingField, value))
+                return false;
+
+            backingField = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
 	}
 	class RelayCommand<T> : ICommand
 	{
