efa7ed7af368049ab255c3bedc89fb200c6265cf
Doan Phuc <mrjared16>
2019-12-04 23:24:22 +0700

    move ControlBar to Views

--- Source/QuanLiQuanCaPhe/QuanLiQuanCaPhe/Views/UserControlBarUC.xaml.cs
+++ Source/QuanLiQuanCaPhe/QuanLiQuanCaPhe/Views/UserControlBarUC.xaml.cs
@@ -1,4 +1,4 @@
-﻿using QuanLiQuanCaPhe.ViewModel;
+﻿using QuanLiQuanCaPhe.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace QuanLiQuanCaPhe.UserControlWPF
+namespace QuanLiQuanCaPhe.Views
 {
 	/// <summary>
 	/// Interaction logic for UserControlBarUC.xaml
